1c8d80e92702f8f2f5ccfe27ba9048fdf8a8a45d
Germey <cqc@cuiqingcai.com>
2019-11-12 04:00:07 +0800

    fit to tf2.0

--- train.py
+++ train.py
@@ -1,21 +1,27 @@
 import tensorflow as tf
 from model_zoo.trainer import BaseTrainer
 from model_zoo.preprocess import standardize
+from model_zoo import flags
 
-tf.flags.DEFINE_integer('epochs', 20, 'Max epochs')
-tf.flags.DEFINE_string('model_class', 'BostonHousingModel', 'Model class name')
-
-
-# tf.flags.DEFINE_integer('steps_per_epoch', 10, help='Steps per epoch while using generator')
+flags.DEFINE_integer('epochs', 20, 'Max epochs')
+flags.DEFINE_string('model_class_name', 'HousePricePredictionModel', 'Model class name')
+flags.DEFINE_string('logger_level', 'INFO', 'Log level')
 
 
 class Trainer(BaseTrainer):
-    
+    """
+    Train Price Prediction Model.
+    """
+
     def prepare_data(self):
-        from tensorflow.python.keras.datasets import boston_housing
-        (x_train, y_train), (x_eval, y_eval) = boston_housing.load_data()
+        """
+        Prepare train data.
+        :return:
+        """
+        (x_train, y_train), (x_eval, y_eval) = tf.keras.datasets.boston_housing.load_data()
         x_train, x_eval = standardize(x_train, x_eval)
         train_data, eval_data = (x_train, y_train), (x_eval, y_eval)
+        self.logger.debug('x_train data shape %s, y_train %s' % (x_train.shape, y_train.shape))
         return train_data, eval_data
 
 
